46e857efdf50939ff3614d016637007d138ad243
Jgmor4es <160650574+Jgmor4es@users.noreply.github.com>
2024-05-02 17:19:03 -0400

    Adicionado exemplo de Template multilenares

---
+++ templatesLiteralsMultilinha.js
@@ -0,0 +1,17 @@
+let textoMultilinha = `
+Esta é uma string
+que ocupa multiplas linhas 
+e voce não precisa se preocupar
+com quebras de linhas ou concatenação.
+`;
+console.log(textoMultilinha);
+/*
+Saída: 
+"
+Esta é uma string 
+que ocupa multiplas linhas
+e voce não precisa se preocupar
+com quebras de linhas ou concatenação
+"
+
+*/
