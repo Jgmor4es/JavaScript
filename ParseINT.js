6c469cfecee740e3f53aec54c03f82105896030a
Jgmor4es <160650574+Jgmor4es@users.noreply.github.com>
2024-05-02 16:33:42 -0400

    Adicionado exemplo de parseInt

---
+++ ParseINT.js
@@ -0,0 +1,8 @@
+let StringParaNumero = "100";
+console.log(StringParaNumero); // Saída: "100"
+console.log(typeof(StringParaNumero)); // Saída: String
+console.log(""); // Saída: "100"
+// Conversão da String "StringParaNumero" para Number
+let numero = parseInt(StringParaNumero);
+console.log(numero); // Saída: 100
+console.log(typeof(numero)); // Saída: Number
