6b0706f74ee9a5d9c30ec9e8a0c4bafcb750ed36
Jgmor4es <160650574+Jgmor4es@users.noreply.github.com>
2024-05-02 17:06:34 -0400

    Adicionado exemplo de Template Literals

---
+++ TemplatesLiterals.js
@@ -0,0 +1,6 @@
+let nome = "João Gabriel"
+let idade = 17;
+
+// Usando template Literals para criar uma string com variáveis 
+let saudacao = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`;
+console.log(saudacao); // Saída: "Olá, meu nome é João Gabriel e eu tenho 17 anos."
