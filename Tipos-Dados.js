17c6c6c5a22bc1a5c9873ce51655308e441037f7
Jgmor4es <160650574+Jgmor4es@users.noreply.github.com>
2024-05-02 16:10:05 -0400

    Adicionado exemplo de dados

---
+++ Tipos-Dados.js
@@ -0,0 +1,13 @@
+let nome = "João";             // Tipo String
+let verdadeiro = true;         // Tipo Boolean
+let falso = false;             // Tipo Boolean
+let numero = 17;               // Tipo Number 
+let reais = 50.00;             // Tipo Number
+let nulo = null;               // Tipo Null
+let indefinido;                // Tipo Undefined 
+let NotANumber = 0 / 0;        // Tipo NaN
+let objeto = {                 // Tipo Object 
+    name: "João",
+    age: 17,
+    Profession: "Student"
+}
