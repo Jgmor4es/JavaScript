80cefc9f4e15b8b80e5637af4698ad9b06927eb7
Jgmor4es <160650574+Jgmor4es@users.noreply.github.com>
2024-05-07 15:43:08 -0400

    Adicionado exemplo sobre comparacao de strings

---
+++ Operadores/comparacao-strings.js
@@ -0,0 +1,8 @@
+let stringsIguais     = "João" == "João"; //true
+let stringsDiferentes = "joão" == "João"; //false
+let stringMenor       = "A" < "B";        //true
+let stringMaior       = "c" > "D";        //false
+console.log(`João == João: ${stringsIguais}`);
+console.log(`joão == João: ${stringsDiferentes}`);
+console.log(`A < B: ${stringMenor}`);
+console.log(`C > D: ${stringMaior}`);
