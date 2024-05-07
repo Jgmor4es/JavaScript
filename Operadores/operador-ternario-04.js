117f604b17b68d7c01483b9788a17f94e3b651d2
Jgmor4es <160650574+Jgmor4es@users.noreply.github.com>
2024-05-07 17:20:25 -0400

    Adicionado exemplo sobre operadores TERNARIOS 4

---
+++ Operadores/operador-ternario-04.js
@@ -0,0 +1,4 @@
+const firstCheck = false;
+const secondCheck =  false;
+const access = firtsCheck ? "Acesso negado" : seondCheck ? "Acesso Negado" : "Acesso conwcedido";
+console.log(access); // Saída: "Acesso concedido"
