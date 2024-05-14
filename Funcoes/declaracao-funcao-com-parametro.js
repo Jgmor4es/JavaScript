92a38297778150b69637627c1344ba74421d84d2
Jgmor4es <160650574+Jgmor4es@users.noreply.github.com>
2024-05-14 13:41:06 -0400

    Adicionado exemplo sobre a função com parametro

---
+++ Funcoes/declaracao-funcao-com-parametro.js
@@ -0,0 +1,7 @@
+function nomeDaFuncao(parametro1, parametro2) {
+    // Corpo da função
+    // Pode conter uma ou mais intruções
+    return resultado; // Opicional
+}
+// Chamada da função
+nomeDaFuncao(parametro1, parametro2);
