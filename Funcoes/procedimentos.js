da14eaa4756cb6d15b6912dc1078dc3e64a33e3c
Jgmor4es <160650574+Jgmor4es@users.noreply.github.com>
2024-05-14 14:12:58 -0400

    Adicionado exemplo sobre procedimentos

---
+++ Funcoes/procedimentos.js
@@ -0,0 +1,5 @@
+function loginErrorMensage() {
+    console.log("Usuário e/ou senha inválidos. Tente novamente ou mais tarde!");
+}
+// Chamando a função para exibir a mensagem
+loginErrorMensage();
